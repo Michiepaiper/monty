7e67eb5e5bae2943b9ee47b75c6ab4d6eb963a6b
Michiepaiper <106969102+Michiepaiper@users.noreply.github.com>
2022-10-06 15:53:14 +0100

    Update push.c

--- push.c
+++ push.c
@@ -1,53 +1,51 @@
-#define GLOBALS
 #include "monty.h"
+#include <ctype.h>
 
 /**
- * push - function that pushes an element to the stack
- * @stack: header of the stack
- * @line_number: number of line in the .m file
- * Return: nothing
+ * check_for_digit - checks that a string only contains digits
+ * @arg: string to check
+ *
+ * Return: 0 if only digits, else 1
  */
-
-void push(stack_t **stack, unsigned int line_number)
+static int check_for_digit(char *arg)
 {
-	stack_t *new, *current;
+	int i;
 
-	if (gbl.num == NULL || is_a_num(gbl.num) == 0)
+	for (i = 0; arg[i]; i++)
 	{
-		dprintf(STDERR_FILENO,
-			"L%d: usage: push integer\n", line_number);
-		free_dlistint(*stack);
-		free(gbl.line);
-		free(gbl.div_line);
-		fclose(gbl.bt_code);
-		exit(EXIT_FAILURE); }
-	new = malloc(sizeof(stack_t));
-	if (new == NULL)
+		if (arg[i] == '-' && i == 0)
+			continue;
+		if (isdigit(arg[i]) == 0)
+			return (1);
+	}
+	return (0);
+}
+
+/**
+ * m_push - push an integer onto the stack
+ * @stack: double pointer to the beginning of the stack
+ * @line_number: script line number
+ *
+ * Return: void
+ */
+void m_push(stack_t **stack, unsigned int line_number)
+{
+	char *arg;
+	int n;
+
+	arg = strtok(NULL, "\n\t\r ");
+	if (arg == NULL || check_for_digit(arg))
 	{
-		dprintf(STDERR_FILENO, "Error: malloc failed\n");
-		free_dlistint(*stack);
-		free(gbl.line);
-		free(gbl.div_line);
-		fclose(gbl.bt_code);
-		exit(EXIT_FAILURE); }
-	new->n = atoi(gbl.num);
-	if (*stack == NULL)
+		dprintf(STDOUT_FILENO,
+			"L%u: usage: push integer\n",
+			line_number);
+		exit(EXIT_FAILURE);
+	}
+	n = atoi(arg);
+	if (!add_node(stack, n))
 	{
-		new->prev = NULL;
-		new->next = NULL;
-		*stack = new; }
-	else if (gbl.mode == 1)
-	{
-		(*stack)->prev = new;
-		new->prev = NULL;
-		new->next = *stack;
-		*stack = new; }
-	else if (gbl.mode == 0)
-	{
-		current = *stack;
-		while (current->next != NULL)
-			current = current->next;
-		new->next = NULL;
-		new->prev = current;
-		current->next = new; }
+		dprintf(STDOUT_FILENO, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	var.stack_len++;
 }
